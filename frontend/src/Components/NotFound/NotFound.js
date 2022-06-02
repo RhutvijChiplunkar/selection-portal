97b8f0be1c3a75d7dc1b11419cb023accf078fb2
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2022-06-02 23:30:27 +0530

    Blue shades applied wherever necessary

--- frontend/src/Components/NotFound/NotFound.js
+++ frontend/src/Components/NotFound/NotFound.js
@@ -5,10 +5,8 @@ const NotFound = () => {
     return ( 
     <div className="coantainer-fluid">
         <main>
-            {/* <h1>4<span><i class="fa fa-snapchat-ghost" aria-hidden="true" spooky/></span>4</h1> */}
-            <h1 classname="notfound-heading"><span>404</span></h1>
-            <h2>Error : 404 page not found</h2>
-            <p>Sorry, the page you're looking for cannot be accessed !</p>
+            <h1 className='error-desc'>Error : 404 page not found</h1>
+            <p className='para'>Sorry, the page you're looking for cannot be accessed !</p>
             <Link to="/Home">Go to Home....</Link>
         </main>  
     </div>
